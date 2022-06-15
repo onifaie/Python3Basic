6b2057db91863db00971ec32306a59edf135f3c1
onifaie <onifaie@gmail.com>
2022-06-15 22:51:50 +0300

    try1

---
+++ Basic1.py
@@ -0,0 +1,21 @@
+#this coment about this command line code 
+
+x1=input("enter the frist number ")
+x2=input ("enter the scand number ")
+print (int(x1)+int(x2))
+
+def result(x):
+    
+    if x<=50:
+        print ("fiuld")
+    if x<=60:
+        print("good")
+    if x<=70:
+        print("very good")
+    if x<=80:
+        print ("exlant")
+    if x<=100:
+        print("Prefict ")
+    
+    
+result(40)    
