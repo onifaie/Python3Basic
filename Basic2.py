8ed5364ceb51872db8b4f2305eea4fce38122f79
onifaie <onifaie@gmail.com>
2022-06-16 14:46:18 +0300

    use cuntuine with if

--- Basic2.py
+++ Basic2.py
@@ -1,5 +1,7 @@
 import time
 x=input("Enter Your years Brith day ")
+r=[12,44,55,66]
+
 def cal(x):
     if int(x)>= 2022:
         print ("your Age very young .....")
@@ -7,3 +9,17 @@ def cal(x):
         print (f"your Age {int(2022)-int(x)} Years Old ")
     
 cal(x)
+
+xy=[10,20,44,66,2]
+
+def addtolist():
+    for i in list(xy):
+        if i == 66:
+            continue
+        else:
+            print (i)
+        
+    
+    
+    
+addtolist()
